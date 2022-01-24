ef12460098d422dab75214ebb8b48a4f32ff2fcc
Brandon  Sangoluisa <bjsangoluisa@espe.edu.ec>
2022-01-24 11:58:37 -0500

    examen_sangoluisa

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
+
+const fs = require('fs');
+//Helpers
+hbs.registerHelper('productos', ()=>{
+    var jsonString = fs.readFileSync("./data/datos.json");
+    var data = JSON.parse(jsonString);
+    var products = "";
+
+    for (x of data) {
+        products +=
+            
+            '<div class="img-plato">'+
+            ' <img src="'+x.Image+'" alt="Hamburguesa" class="img-responsive"></div><div class="desc-plato-menu"><h4>'+x.Name+'</h4><p class="precio-plato">'+x.cost+'</p><p class="desc-plato">Lorem ipsum dolor sit amet.</p><br><a href="#" class="boton boton-color">Ordenar</a></div>  '                
+
+        ;             
+    }
+
+    return new hbs.SafeString(products);
+});
+
 app.use(express.static(__dirname + '/public'));
 
 //Motor de vistas de HBS de express
